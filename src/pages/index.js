c11c16ed1a0c7a964aa28ec8582c9f4bc10f4b4e
Mitesh Shah <mitesh@mokko.io>
2018-11-14 23:45:32 -0600

    Curves example.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <section className="hero is-large is-primary">
+    <section className="hero curved-bg has-text-white">
       <div className="hero-body">
         <div className="container has-text-centered">
-          <h1 className="title">Vertically Centered Text anywhere</h1>
-          <h2 className="subtitle">Subtitle</h2>
+          <h1 className="title has-text-white">
+            Vertically Centered Text anywhere
+          </h1>
+          <h2 className="subtitle has-text-white">Subtitle</h2>
         </div>
       </div>
     </section>
